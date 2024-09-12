fc0e446dc4201fdf829adb84cccd2dc9b8efc855
shekhar shinde <94914531+shekharshinde-161@users.noreply.github.com>
2024-09-12 12:38:33 +0530

    LinkedList

---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructure
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            List list = new List();
+            list.InsertAtBegin(10);
+            list.InsertAtBegin(20);
+            list.InsertAtBegin(30);
+
+            list.PrintList();
+
+            list.InsertAtPos(50, 3);
+
+            list.InsertAtEnd(40);
+            list.PrintList();
+
+            list.RemoveAtBegin();
+            list.PrintList();
+        }
+    }
+}
+
+
+//insert after   data,ref
+
+//remove end 
+
+//remove data
+
+//length -- counter 0  
+
+//find  -- return bool 
